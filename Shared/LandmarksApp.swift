65dff1da4fb19cbd7cb210a8db29168813b6d887
Abidemi Ope <opeabidemi@gmail.com>
2022-07-19 16:46:11 +0100

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Harry on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
